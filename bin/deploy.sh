81fc8df8b9b2ae54f45df98307ec913c1bf292f3
jaequery <jaequery@gmail.com>
2017-04-28 21:35:33 -0700

    set back to hak server

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,5 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 parentdir="$(dirname "$dir")"
 app=${parentdir##*/}
 dest=$1
-dest="root@techbeach"
+dest="jae@x.hakberry.com"
 rsync -avzr $parentdir $dest:~/sites/ && ssh $dest "cd ~/sites/$app && docker-compose stop && docker-compose -f docker-compose.live.yml up && docker-compose logs"
